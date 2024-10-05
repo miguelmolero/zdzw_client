cbe78cef7e3a54ffe044426ec72f2fb6f991b319
Alejandro Cambronero <acambronero@innerspec.com>
2024-10-05 18:13:09 +0200

    Add Strip Chart Canvas component

---
+++ src/components/StripChartCanvas.tsx
@@ -0,0 +1,45 @@
+// src/components/ChartCanvas.tsx
+import React, { useEffect, useRef } from 'react';
+import { Chart, ChartData, ChartOptions } from 'chart.js';
+// src/components/StripChartCanvas.tsx
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  BarController,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+// Registrar los componentes
+ChartJS.register(BarController, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
+interface StripChartCanvasProps {
+  type: 'bar' | 'line' | 'pie' | 'doughnut' | 'radar'; // Tipos de gráfico admitidos
+  data: ChartData; // Datos que se pasarán al gráfico
+  options?: ChartOptions; // Opciones de configuración adicionales para Chart.js
+}
+
+const StripChartCanvas: React.FC<StripChartCanvasProps> = ({ type, data, options }) => {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const ctx = canvasRef.current!.getContext('2d');
+    const chartInstance = new Chart(ctx!, {
+      type,
+      data,
+      options,
+    });
+
+    // Limpiar el gráfico cuando el componente se desmonte
+    return () => {
+      chartInstance.destroy();
+    };
+  }, [type, data, options]);
+
+  return <canvas ref={canvasRef}/>;
+};
+
+export default StripChartCanvas;
