f86d4c938155036a2e06f9213b90947c729f4752
Alejandro Cambronero <acambronero@innerspec.com>
2024-10-12 21:30:41 +0200

    Use static base only for production

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,13 @@ import react from '@vitejs/plugin-react'
 import * as dotenv from 'dotenv';
 
 dotenv.config();
+const isProduction = process.env.NODE_ENV === 'production';
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/static/',
+  base: isProduction ? '/static/' : '/',
   server: {
     port: parseInt(process.env.VITE_PORT || '3000'),
   },
