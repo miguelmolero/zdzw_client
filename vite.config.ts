00a0850e8af850189f3218ba38cd1ee83c09ed66
Alejandro Cambronero <acambronero@innerspec.com>
2024-11-30 17:47:10 +0100

    Use Full-Reload vite plugin to reload all the project and visualize the real time changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fullReload from 'vite-plugin-full-reload';
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -8,9 +9,19 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    fullReload('./**/*', {
+      root: __dirname,
+      delay: 0,
+      always: true
+    })
+  ],
   base: isProduction ? '/static/' : '/',
   server: {
     port: parseInt(process.env.VITE_CLIENT_PORT || '3000'),
+    hmr: {
+      overlay: false,
+    },
   },
 })
