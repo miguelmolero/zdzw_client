637ae2f51aa75f0a7f021fa74d9396ebcb792d17
Alejandro Cambronero <acambronero@innerspec.com>
2024-12-19 12:23:19 +0100

    Add navigation to new pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import PrivateRoute from './components/PrivateRoute';
 import Login from './pages/Login';
 import EntryMenu from './pages/EntryMenu';
 import InspectionVisualizator from './pages/InspectionVisualizator';
+import FactoryMetrics from './pages/FactoryMetrics';
+import WeldingProcessQuality from './pages/WeldingProcessQuality';
 
 const App: React.FC = () => {
   return (
@@ -14,6 +16,8 @@ const App: React.FC = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/entry-menu" element={<PrivateRoute><EntryMenu /></PrivateRoute>} />
           <Route path="/inspection-visualizator" element={<PrivateRoute><InspectionVisualizator/></PrivateRoute>} />
+          <Route path="/factory-metrics" element={<PrivateRoute><FactoryMetrics/></PrivateRoute>} />
+          <Route path="/welding-process-quality" element={<PrivateRoute><WeldingProcessQuality/></PrivateRoute>} />
           <Route path="*" element={<Navigate to="/login" />} />
         </Routes>
       </Router>
