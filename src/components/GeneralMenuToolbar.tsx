16407d7fd278ffe432d07b434db81425db1afea9
Alejandro Cambronero <acambronero@innerspec.com>
2024-12-19 12:34:04 +0100

    Change name to General toolbar

--- src/components/GeneralMenuToolbar.tsx
+++ src/components/GeneralMenuToolbar.tsx
@@ -4,7 +4,7 @@ import { Box, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import HomeIcon from '@mui/icons-material/Home';
 import SettingsIcon from '@mui/icons-material/Settings';
-import { toolbarStyles } from './styles/ToolbarStyles'; // Importar estilos de la toolbar
+import { toolbarStyles } from './styles/GeneralMenuToolbarStyles'; // Importar estilos de la toolbar
 
 const Toolbar : React.FC = () => {
   const styles = toolbarStyles();
