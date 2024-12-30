0cb13d300d7f7f196d4032db26be70178c7f41dc
Raphael Pinto <rpinto@innerspec.com>
2024-12-30 11:25:10 +0100

    Navigate to entry menu on Home button click

--- src/components/GeneralMenuToolbar.tsx
+++ src/components/GeneralMenuToolbar.tsx
@@ -1,5 +1,5 @@
-// src/components/Toolbar.tsx
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import { Box, IconButton } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import HomeIcon from '@mui/icons-material/Home';
@@ -7,12 +7,13 @@ import SettingsIcon from '@mui/icons-material/Settings';
 import { toolbarStyles } from './styles/GeneralMenuToolbarStyles'; // Importar estilos de la toolbar
 
 const Toolbar : React.FC = () => {
+  const navigate = useNavigate();
   const styles = toolbarStyles();
 
   return (
     <Box sx={styles.sidebar}>
       <IconButton><MenuIcon /></IconButton>
-      <IconButton><HomeIcon /></IconButton>
+      <IconButton onClick={() => {navigate("/entry-menu")}}><HomeIcon /></IconButton>
       <IconButton><SettingsIcon /></IconButton>
     </Box>
   );
