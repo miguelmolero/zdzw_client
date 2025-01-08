2c485557fe8b044aec2b5ba6611ade5e422a2b1b
Alejandro Cambronero <acambronero@innerspec.com>
2025-01-08 01:39:22 +0100

    Add table for card component

--- src/components/StatsView/StatsData.tsx
+++ src/components/StatsView/StatsData.tsx
@@ -1,18 +1,55 @@
 import React from "react";
-import { StatsDataItem } from "./styles/StatsViewStyles";
+import {StatsData} from "../../types/statistics_types";
+import { 
+    StatsDataItem,
+    StatsDataTable,
+    StatsTableHead,
+    StatsTableBody,
+    StatsTableRow,
+    StatsTableCell,
+} from "./styles/StatsViewStyles";
 
-const StatsData : React.FC = () => {
+interface StatsInfoDataProps {
+    data: StatsData;
+}
+
+const StatsInfoData : React.FC<StatsInfoDataProps> = ({data}) => {
 
     return (
         <StatsDataItem>
-            <span>Factory Name</span>
-            <span>Factory ID</span>
-            <span>All | 1000 | 100%</span>
-            <span>Pass | 900 | 90%</span>
-            <span>Fail | 90 | 9%</span>
-            <span>Invalid | 10 | 1%</span>
+            <StatsDataTable>
+                <StatsTableHead>
+                    <StatsTableRow>
+                        <StatsTableCell></StatsTableCell>
+                        <StatsTableCell>Count</StatsTableCell>
+                        <StatsTableCell>Rate(%)</StatsTableCell>
+                    </StatsTableRow>
+                </StatsTableHead>
+                <StatsTableBody>
+                    <StatsTableRow>
+                        <StatsTableCell>Pass</StatsTableCell>
+                        <StatsTableCell>{data.pass_count}</StatsTableCell>
+                        <StatsTableCell>{data.pass_rate.toFixed(2)}</StatsTableCell>
+                    </StatsTableRow>
+                    <StatsTableRow>
+                        <StatsTableCell>Fail</StatsTableCell>
+                        <StatsTableCell>{data.fail_count}</StatsTableCell>
+                        <StatsTableCell>{data.fail_rate.toFixed(2)}</StatsTableCell>
+                    </StatsTableRow>
+                    <StatsTableRow>
+                        <StatsTableCell>Invalid</StatsTableCell>
+                        <StatsTableCell>{data.invalid_count}</StatsTableCell>
+                        <StatsTableCell>{data.invalid_rate.toFixed(2)}</StatsTableCell>
+                    </StatsTableRow>
+                    <StatsTableRow>
+                        <StatsTableCell>Total</StatsTableCell>
+                        <StatsTableCell>{data.total_count}</StatsTableCell>
+                        <StatsTableCell>{Math.round(data.pass_rate + data.fail_rate + data.invalid_rate)}</StatsTableCell>
+                    </StatsTableRow>
+                </StatsTableBody>
+            </StatsDataTable>
         </StatsDataItem>
     )
 };
 
-export default StatsData;
+export default StatsInfoData;
