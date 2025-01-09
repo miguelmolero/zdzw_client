b4045cafc826cad25a433b0c3047c9b87b66e840
Alejandro Cambronero <acambronero@innerspec.com>
2025-01-09 11:08:45 +0100

    Change WeldingProcessQuality page like FactoryMetrics page

--- src/pages/WeldingProcessQuality.tsx
+++ src/pages/WeldingProcessQuality.tsx
@@ -1,16 +1,43 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {
     WPQMainContentContainer,
+    CanvasContainerStats
 } from "./styles/WeldingProcessQualityStyles" // Importar los estilos
 import Toolbar from "../components/GeneralMenuToolbar"; // Importar la Toolbar reutilizable
 import DefaultLayout from "../components/DefaultLayout";
+import SettingsColumn from "../components/SettingsColumn/SettingsColumn";
+import StatsView from "../components/StatsView/StatsView";
+import { useApplicationTypeContext } from "../context/ApplicationTypeContext";
+import { useStatisticsContext} from "../context/StatisticsContext";
+import { useDataHandlerContext } from "../context/DataHandlerContext";
 
 const WeldingProcessQuality: React.FC = () => {
+    const { applicationType } = useApplicationTypeContext();
+    const {selectedFactory, inspectionFilters, setInspectionFilters} = useDataHandlerContext();
+    const { statisticsData, getStatisticsData } = useStatisticsContext();
+
+    useEffect(() => {
+        if (statisticsData.factory_stats.length != 0) return;
+        getStatisticsData();
+    }, [inspectionFilters]);
+
+    useEffect(() => {
+        if (statisticsData.factory_stats.length != 0) return;
+        setInspectionFilters({
+            ...inspectionFilters,
+            factory_id: selectedFactory
+        });
+        getStatisticsData();
+    }, [applicationType]);
 
     return (
         <DefaultLayout>
             <Toolbar />
             <WPQMainContentContainer>
+                <SettingsColumn />
+                <CanvasContainerStats>
+                    <StatsView data={statisticsData}/>
+                </CanvasContainerStats>
             </WPQMainContentContainer>
         </DefaultLayout>
     )
--- src/pages/styles/WeldingProcessQualityStyles.ts
+++ src/pages/styles/WeldingProcessQualityStyles.ts
@@ -4,4 +4,15 @@ import { styled, Box} from "@mui/material";
 export const WPQMainContentContainer = styled(Box)({
     flexGrow: 1,
     backgroundColor: '#e0f7e9', // Fondo verde suave detrás de las cajas
+    display: 'flex',
+});
+
+export const CanvasContainerStats = styled(Box)({
+    flex: 1,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignSelf: 'flex-start',
+    height: '100%',
+    overflow: 'auto',
 });
