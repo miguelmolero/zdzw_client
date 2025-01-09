94758c6332f922ddf8404fa2e0ac224ad1872265
Alejandro Cambronero <acambronero@innerspec.com>
2025-01-09 11:53:05 +0100

    Show device data when apptype is WPQ

--- src/components/StatsView/StatsView.tsx
+++ src/components/StatsView/StatsView.tsx
@@ -1,25 +1,52 @@
 import React from "react";
-import { useMediaQuery, Theme } from "@mui/material";
+import { Typography } from "@mui/material";
 import { 
     StatsContainer,
+    StatsColumnContainer
 } from "./styles/StatsViewStyles";
 import StatsCard from "./StatsCard";
 import { StatisticsData, StatsData, FactoryStatsData } from "../../types/statistics_types";
 import {ApplicationType} from "../../types/application_types";
 import { useApplicationTypeContext } from "../../context/ApplicationTypeContext";
-
+import {useDataHandlerContext} from "../../context/DataHandlerContext";
 interface StatsViewProps {
     data: StatisticsData;
 }
 
 const StatsView : React.FC<StatsViewProps> = ({data}) => {
     const { applicationType } = useApplicationTypeContext();
+    const { selectedFactory } = useDataHandlerContext();
+    const isWPQApp = applicationType === ApplicationType.WeldingProcessQuality;
+
+    if (isWPQApp) {
+        let factory_number = 0;
+        if (selectedFactory == -1) {
+            factory_number = 0;
+        }else{
+            factory_number = selectedFactory-1;
+        }
+
+        return (
+            <StatsColumnContainer>
+                <Typography sx={{textDecoration: "underline"}} align="center" variant="h5" component="h5" >
+                        Factory ID - {factory_number+1}
+                </Typography>
+                <StatsContainer>
+                    {
+                        data.factory_stats[factory_number]?.device_stats?.map((stats: StatsData) => (
+                            <StatsCard key={stats.id} data={stats}/>
+                        ))
+                    }
+                </StatsContainer>
+            </StatsColumnContainer>
+        )
+    }
 
     return (
         <StatsContainer>
             {
                 data.factory_stats.map((factory: FactoryStatsData) => (
-                    <StatsCard key={factory.factory_data.id} data={factory.factory_data} n_columns={3}/>
+                    <StatsCard key={factory.factory_data.id} data={factory.factory_data}/>
                 ))
             }
         </StatsContainer>
