cd2f4e399b876145f3a6df701bbbbdf1df49291b
Alejandro Cambronero <acambronero@innerspec.com>
2025-01-09 11:52:35 +0100

    Change styles to have a responsive card list

--- src/components/StatsView/styles/StatsViewStyles.ts
+++ src/components/StatsView/styles/StatsViewStyles.ts
@@ -1,65 +1,50 @@
 import { styled, Box, Table, TableHead, TableBody, TableRow, TableCell } from "@mui/material";
 
-export const StatsContainer = styled(Box)({
-    // flex: 1,
-    // display: 'flex',
-    // flexDirection: 'column',
-    // justifyContent: 'flex-start',
-    // alignSelf: 'flex-start',
-    // height: '100%',
-    // width: '100%',
+export const StatsColumnContainer = styled(Box)({
     display: 'flex',
-    flexWrap: 'wrap',
-    justifyContent: 'left',
-    alignContent: 'flex-start',
-    gap: '5px',
-    height: '100%',
+    flexDirection: 'column',
     width: '100%',
+    height: '100%',
+});
+
+export const StatsContainer = styled(Box)({
+    display: 'grid',
+    gridTemplateColumns: 'repeat(auto-fit, minmax(450px, 1fr))',
+    gridAutoRows: '60%',
+    gap: '5px',
     padding: '5px',
 });
 
-export const StatsItem = styled(Box)(({ columns }: { columns: number }) => ({
-    flex: `0 0 calc(${100 / columns}% - 16px)`,
-    minWidth: "150px", // Tamaño mínimo para evitar cajas pequeñas
+export const StatsItemColumn = styled(Box)({
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+    padding: "16px",
     backgroundColor: "white",
     borderRadius: "8px",
     boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1)",
-    padding: "16px",
+    "&:hover": {
+        backgroundColor: "rgba(0, 255, 0, 0.2)", // Cambio de color al pasar el ratón
+    },
+});
+
+export const StatsItem = styled(Box)({
     display: "flex",
     textAlign: "center",
     alignContent: "center",
     flexDirection: "row",
     justifyContent: "center",
     alignSelf: "center",
-    height: "35vh",
+    height: "100%",
     width: "100%",
-    "&:hover": {
-        backgroundColor: "rgba(0, 255, 0, 0.2)", // Cambio de color al pasar el ratón
-    },
-}));
-
-// export const StatsItem = styled(Box)({
-//     display: 'flex',
-//     justifyContent: 'flex-start',
-//     alignItems: 'flex-start',
-//     border: '1px solid #ccc',
-//     gap: '16px',
-//     padding: '16px',
-//     height: '30vh',
-//     width: '40%',
-
-// });
+});
 
 export const PieChartContainer = styled(Box)({
-    // display: 'flex',
-    // flexDirection: 'column',
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    // width: '100%',
-    // height: '100%',
-    display: 'flex',
-    width: '60%',
-    height: '70%',
+    width: '50%',
+    height: '75%',
     alignItems: 'center',
     justifyContent: 'center',
     alignSelf: 'center',
@@ -70,39 +55,34 @@ export const StatsDataItem = styled(Box)({
     alignContent: 'center',
     justifyContent: 'center',
     alignSelf: 'center',
-    // height: '40%',
-    // width: '40%',
+    width: '100%',
+    height: '100%',
 });
 
 export const StatsDataTable = styled(Table)({
-    //display: 'flex',
     alignContent: 'center',
     justifyContent: 'center',
     alignSelf: 'center',
-    height: '100%',
-    width: '100%',
+    height: '75%',
+    width: '90%'
 });
 
 export const StatsTableHead = styled(TableHead)({
-    //display: 'flex',
     alignContent: 'center',
     justifyContent: 'center',
 });
 
 export const StatsTableBody = styled(TableBody)({
-    //display: 'flex',
     alignContent: 'center',
     justifyContent: 'center',
 });
 
 export const StatsTableRow = styled(TableRow)({
-    //display: 'flex',
     alignContent: 'center',
     justifyContent: 'center',
 });
 
 export const StatsTableCell = styled(TableCell)({
-    //display: 'flex',
     alignContent: 'center',
     justifyContent: 'center',
 });
