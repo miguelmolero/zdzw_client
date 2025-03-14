817cc8132098e493db49e5973208eb3d59eb5889
Alejandro Cambronero <acambronero@innerspec.com>
2025-03-14 13:37:25 +0100

    Change axios config to get the correct url in production and development

--- src/api/axiosConfig.ts
+++ src/api/axiosConfig.ts
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import { apiRoutes } from './apiRoutes';
-//import CONFIG from '../config/config'
+import CONFIG from '../config/config'
 
-//const isProduction = process.env.NODE_ENV === 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 const api = axios.create({
-  baseURL: window.location.origin //CONFIG.VITE_API_PROD_URL : CONFIG.VITE_API_URL  // Asegúrate de que la URL del backend sea correcta
+  baseURL: isProduction ? window.location.origin : CONFIG.VITE_API_URL  // Asegúrate de que la URL del backend sea correcta
 });
 
 // Interceptor para añadir el token en el encabezado de autorización
